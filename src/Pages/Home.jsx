7a89c9af2becbd9ece734d1581b238f55106b81d
Harsh99830 <harshagrawal7878@gmail.com>
2025-12-15 00:18:14 +0530

    loading bar at center now

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -73,7 +73,7 @@ const Home = ({ sidebarOpen, onToggleSidebar }) => {
 
   if (loading) {
     return (
-      <div className="flex items-center justify-center min-h-[60vh]">
+      <div className="fixed inset-0 flex items-center justify-center bg-white z-50">
         <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-[#c2b490]"></div>
       </div>
     );
